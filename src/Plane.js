b9266a78d7c5234281133a4f2d2bc71e4d0e90b6
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-10-24 17:15:49 +0100

    Passing tests for ClearForTakeOff

--- src/Plane.js
+++ src/Plane.js
@@ -1,9 +1,11 @@
+
 'use strict';
 
-
-function Plane (){}
-
-
-Plane.prototype.land = function(airport) {
+function Plane(){}
+Plane.prototype.land = function(airport){
   airport.clearForLanding(this);
+  this._location = airport;
+};
+Plane.prototype.takeoff = function(airport) {
+  this._location.clearForTakeOff('');
 };
